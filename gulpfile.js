40cf637bb1422844d1e049ffc771b980a75a47f5
ivanvolosevich <ivan.a.volosevich@mail.ru>
2020-11-29 16:17:59 +0300

    29.11.2020

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
-const browserSync = require('browser-sync');
-const browserSunc = require('browser-sync').create;
+const browserSync = require('browser-sync').create();
 
 function styles() {
     return gulp.src('src/scss/main.scss')
         .pipe(sass())
+        // .pipe(cssnano())
         .pipe(rename('styles.css'))
         .pipe(gulp.dest('./css/'))
         .pipe(browserSync.stream());
@@ -16,12 +16,13 @@ function styles() {
 function watch() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./",
             index: "./index.html"
         }
     });
     gulp.watch('src/scss/**/*.scss', styles)
     gulp.watch('index.html').on('change', browserSync.reload);
+
 }
 
 exports.watch = watch;
